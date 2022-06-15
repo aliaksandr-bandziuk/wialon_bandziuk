9266017ed737a71490bc538e36c7e42b1a2abf7d
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-06-15 20:01:59 +0300

    feat: create description

---
+++ src/Description/Description.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Description.scss';
+
+const Description = () => {
+  return (
+    <section className="description">
+      
+    </section>
+  )
+}
+
+export default Description;
