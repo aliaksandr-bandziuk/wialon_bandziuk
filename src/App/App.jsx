ffbc4869bf8dacc9bd1a2b4ed684c6a9538494a2
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-06-15 14:34:31 +0300

    initial

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './App.scss';
+
+import Header from "../Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
